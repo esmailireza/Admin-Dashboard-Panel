732cc7b88fde1b0e894a192fa4cb3d0c13b95e4e
esmailireza <esmailireza20@gmail.com>
2022-05-05 00:58:08 +0430

    implement sidebar and navbar component

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,8 +1,15 @@
+import Navbar from "../../components/navbar/navbar";
+import Sidebar from "../../components/sidebar/sidebar";
 import "./home.scss"
 
 const Home = () => {
     return ( 
-        <div className="home">home</div>
+        <div className="home">
+            <Sidebar/>
+            <div className="homeContainer">
+                <Navbar/>
+            </div>
+        </div>
      );
 }
  
